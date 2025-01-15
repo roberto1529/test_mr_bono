e9095dcbe7d7b63c0214c13cb62046cab578b12e
Roberto Molina Gonzalez <45692969+roberto1529@users.noreply.github.com>
2025-01-15 11:53:34 -0500

    monorepo

--- apps/shared/src/shared.service.ts
+++ apps/shared/src/shared.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class SharedService {
   getHello(): string {
-    return 'Hello World! primario';
+    return 'Hello World!';
   }
 }
